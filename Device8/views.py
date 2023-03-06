cd8238e9b57618b62f37861df85dad4ed4f97390
jimvla <jimvlachos14@gmail.com>
2023-03-06 15:40:48 +0200

    Adding some comments

--- Device8/views.py
+++ Device8/views.py
@@ -9,7 +9,7 @@ import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
 
-# Create your views here.
+# Home view for our page
 def home(request):
     if request.method=="POST":
         dropdown = request.POST.get('dropdown')
@@ -19,7 +19,7 @@ def home(request):
         data = pd.DataFrame(Queryset)
         data['time'] = pd.to_datetime(data['time'])
 
-        #Check if user provide correct dates
+        #Check if user provide dates that exist in our data
         if data.empty:
             plot_div = "No Data Found for that Date Range. Please try different values"
 
@@ -43,19 +43,23 @@ def home(request):
         data.drop(upper[0], inplace = True)
         data.drop(lower[0], inplace = True)
 
+        # New DataFrame to help us with the Candlestick Plot
         new_df = pd.DataFrame({
-        'date': data['time'].dt.date.unique(),
-        'open': data.groupby(data['time'].dt.date)['PM1'].first(),
-        'close': data.groupby(data['time'].dt.date)['PM1'].last(),
-        'high': data.groupby(data['time'].dt.date)['PM1'].max(),
-        'low': data.groupby(data['time'].dt.date)['PM1'].min(),
+        'date': data.groupby(pd.Grouper(key='time', freq='H'))['time'].first(),
+        'open': data.groupby(pd.Grouper(key='time', freq='H'))['PM1'].first(),
+        'close': data.groupby(pd.Grouper(key='time', freq='H'))['PM1'].last(),
+        'high': data.groupby(pd.Grouper(key='time', freq='H'))['PM1'].max(),
+        'low': data.groupby(pd.Grouper(key='time', freq='H'))['PM1'].min(),
         })
 
+        # Trends using Rolling Window and EMA
         new_df['4wma'] = new_df['close'].rolling(window=3).mean()
-        new_df['wma'] = new_df['close'].rolling(window=5).mean()
-        # Reset index of the new dataframe
+        new_df['wma'] = new_df['close'].ewm(span=10).mean()
+
+        # Reset index of the new DataFrame
         new_df = new_df.reset_index(drop=True)
 
+        # Candlestick Trace and Layout
         trace = go.Candlestick(
             x=new_df['date'],
             open=new_df['open'],
@@ -78,19 +82,9 @@ def home(request):
             template = 'plotly_dark'
         )
                
-        #Calculate Rolling Mean
-        #rolling_mean = data[dropdown].rolling(window=100).mean()
-        #ema10 = data[dropdown].ewm(span=10).mean()
-        #ema25 = data[dropdown].ewm(span=25).mean()
-        #ema99 = data[dropdown].ewm(span=99).mean()    
-        #trace1 = go.Scatter(x=data['time'], y=data[dropdown], name=dropdown)
-        #trace2 = go.Scatter(x=data['time'], y=rolling_mean, line=dict(color='yellow', width=1), name='TREND')
-        #fig.add_scatter(x=data['time'], y=ema25, line=dict(color='pink', width=2), name='EMA(25)')
-        #fig.add_scatter(x=data['time'], y=ema99, line=dict(color='purple', width=2), name='EMA(99)')
         candle = [trace]
-        #traces = [trace1, trace2]
-    
-        #Plot
+
+        # Plot Figure
         fig = go.Figure(data=candle, layout=layout)
         fig.add_trace(go.Scatter(x=new_df['date'], y=new_df['4wma'], line=dict(color = 'yellow'), name= "Moving Average"))
         fig.add_trace(go.Scatter(x=new_df['date'], y=new_df['wma'], line=dict(color = 'pink'), name= "Moving Average New"))
@@ -102,7 +96,7 @@ def home(request):
             
         return render(request, "home.html", context)
     else:
-        #Homepage initial message
+        # Homepage initial message before plotting the graph
         plot_div = "Your Plot is going here! Just choose a Start Date and an End Date to plot your chart"
 
         context = {
@@ -110,6 +104,7 @@ def home(request):
             }
         return render(request, "home.html", context)
 
+# View for User Login and Authentication
 def login_user(request):
     if request.method == "POST":
         username = request.POST['username']
