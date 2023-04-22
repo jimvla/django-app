3434c1ce66f4e1c636c9a680fe2575764271fba7
jimvla <jimvlachos14@gmail.com>
2023-04-22 20:11:23 +0300

    new update

--- Device8/views.py
+++ Device8/views.py
@@ -12,13 +12,16 @@ import numpy as np
 # Home view for our page
 def home(request):
     if request.method=="POST":
-        # GET INPUTS
-        dropdown = request.POST.get('dropdown')
-        radio = request.POST.get('inlineRadioOptions')
-        MA = request.POST.get('inlineCheckbox1')
-        EMA = request.POST.get('inlineCheckbox2')       
+        # Get Inputs
+        dropdown = request.POST.get('dropdown') # PM Value
+        radio = request.POST.get('inlineRadioOptions') # Frequency Selection
+        MA = request.POST.get('inlineCheckbox1') 
+        EMA = request.POST.get('inlineCheckbox2')      
         start_date = request.POST.get('start_date')
         end_date = request.POST.get('end_date')
+        outliers = request.POST.get('inlineRadioOptions1') # Keep or Delete Outliers
+
+        # Get Query from DB
         Queryset = Device8.objects.filter(time__range=[start_date, end_date]).values()
         data = pd.DataFrame(Queryset)
         data['time'] = pd.to_datetime(data['time'])
@@ -32,94 +35,98 @@ def home(request):
             }
             return render(request, "home.html", context)
         
-        #Checking and Drop Outliers
-        Q1 = data[dropdown].quantile(0.25)
-        Q3 = data[dropdown].quantile(0.75)
-
-        IQR = Q3 - Q1
-
-        # Upper bound
-        upper = np.where(data[dropdown] >= (Q3+1.5*IQR))
-        # Lower bound
-        lower = np.where(data[dropdown] <= (Q1-1.5*IQR))
-        
-        # Removing the Outliers
-        data.drop(upper[0], inplace = True)
-        data.drop(lower[0], inplace = True)
-
-        # New DataFrame to help us with the Candlestick Plot
-        if dropdown == 'PM1':
-            new_df = pd.DataFrame({
-            'date': data.groupby(pd.Grouper(key='time', freq=radio))['time'].first(),
-            'open': data.groupby(pd.Grouper(key='time', freq=radio))['PM1'].first(),
-            'close': data.groupby(pd.Grouper(key='time', freq=radio))['PM1'].last(),
-            'high': data.groupby(pd.Grouper(key='time', freq=radio))['PM1'].max(),
-            'low': data.groupby(pd.Grouper(key='time', freq=radio))['PM1'].min(),
-            })
-        elif dropdown == 'PM25':
-            new_df = pd.DataFrame({
-            'date': data.groupby(pd.Grouper(key='time', freq=radio))['time'].first(),
-            'open': data.groupby(pd.Grouper(key='time', freq=radio))['PM25'].first(),
-            'close': data.groupby(pd.Grouper(key='time', freq=radio))['PM25'].last(),
-            'high': data.groupby(pd.Grouper(key='time', freq=radio))['PM25'].max(),
-            'low': data.groupby(pd.Grouper(key='time', freq=radio))['PM25'].min(),
-            })
         else:
-            new_df = pd.DataFrame({
-            'date': data.groupby(pd.Grouper(key='time', freq=radio))['time'].first(),
-            'open': data.groupby(pd.Grouper(key='time', freq=radio))['PM10'].first(),
-            'close': data.groupby(pd.Grouper(key='time', freq=radio))['PM10'].last(),
-            'high': data.groupby(pd.Grouper(key='time', freq=radio))['PM10'].max(),
-            'low': data.groupby(pd.Grouper(key='time', freq=radio))['PM10'].min(),
-            })    
+
+            if outliers == "True":
+                # Checking and Drop Outliers
+                Q1 = data[dropdown].quantile(0.25)
+                Q3 = data[dropdown].quantile(0.75)
+
+                IQR = Q3 - Q1
+
+                # Upper bound
+                upper = np.where(data[dropdown] >= (Q3+1.5*IQR))
+                # Lower bound
+                lower = np.where(data[dropdown] <= (Q1-1.5*IQR))
+                
+                # Removing the Outliers
+                data.drop(upper[0], inplace = True)
+                data.drop(lower[0], inplace = True)
+
+            # New DataFrame for the Candlestick Plot
+            if dropdown == 'PM1':
+                new_df = pd.DataFrame({
+                'date': data.groupby(pd.Grouper(key='time', freq=radio))['time'].first(),
+                'open': data.groupby(pd.Grouper(key='time', freq=radio))['PM1'].first(),
+                'close': data.groupby(pd.Grouper(key='time', freq=radio))['PM1'].last(),
+                'high': data.groupby(pd.Grouper(key='time', freq=radio))['PM1'].max(),
+                'low': data.groupby(pd.Grouper(key='time', freq=radio))['PM1'].min(),
+                })
+            elif dropdown == 'PM25':
+                new_df = pd.DataFrame({
+                'date': data.groupby(pd.Grouper(key='time', freq=radio))['time'].first(),
+                'open': data.groupby(pd.Grouper(key='time', freq=radio))['PM25'].first(),
+                'close': data.groupby(pd.Grouper(key='time', freq=radio))['PM25'].last(),
+                'high': data.groupby(pd.Grouper(key='time', freq=radio))['PM25'].max(),
+                'low': data.groupby(pd.Grouper(key='time', freq=radio))['PM25'].min(),
+                })
+            else:
+                new_df = pd.DataFrame({
+                'date': data.groupby(pd.Grouper(key='time', freq=radio))['time'].first(),
+                'open': data.groupby(pd.Grouper(key='time', freq=radio))['PM10'].first(),
+                'close': data.groupby(pd.Grouper(key='time', freq=radio))['PM10'].last(),
+                'high': data.groupby(pd.Grouper(key='time', freq=radio))['PM10'].max(),
+                'low': data.groupby(pd.Grouper(key='time', freq=radio))['PM10'].min(),
+                })    
 
         
-        # Reset index of the new DataFrame
-        new_df = new_df.reset_index(drop=True)
+            # Reset index of the new DataFrame
+            new_df = new_df.reset_index(drop=True)
 
-        # Candlestick Trace and Layout
-        trace = go.Candlestick(
-            x=new_df['date'],
-            open=new_df['open'],
-            high=new_df['high'],
-            low=new_df['low'],
-            close=new_df['close'],
-            name='Candestick'
-        )
+            # Candlestick Trace and Layout
+            trace = go.Candlestick(
+                x=new_df['date'],
+                open=new_df['open'],
+                high=new_df['high'],
+                low=new_df['low'],
+                close=new_df['close'],
+                name='Candestick'
+            )
 
-        layout = go.Layout(
-            title='Trend Line of '+ dropdown + '(ug/m3) over time',
-            xaxis_title='Time',
-            yaxis_title= dropdown + '(ug/m3)',
-            xaxis=dict(rangeslider=dict(visible=False)),
-            font=dict(
-                family="Courier New, monospace",
-                size=18,
-                color="RebeccaPurple"
-            ),
-            template = 'plotly_dark'
-        )
-               
-        candle = [trace]
+            layout = go.Layout(
+                title='Trend Line of '+ dropdown + '(ug/m3) over time',
+                xaxis_title='Time',
+                yaxis_title= dropdown + '(ug/m3)',
+                xaxis=dict(rangeslider=dict(visible=False)),
+                font=dict(
+                    family="Courier New, monospace",
+                    size=18,
+                    color="RebeccaPurple"
+                ),
+                template = 'plotly_dark'
+            )
+                
+            candle = [trace]
 
-        # Plot Figure + Trends using Rolling Window and EMA
-        fig = go.Figure(data=candle, layout=layout)
-        if MA:      
-            new_df['ma'] = new_df['close'].rolling(window=3).mean()
-            fig.add_trace(go.Scatter(x=new_df['date'], y=new_df['ma'], line=dict(color = 'yellow'), name= "Moving Average"))
-        if EMA:
-            new_df['ema'] = new_df['close'].ewm(span=10).mean()
-            fig.add_trace(go.Scatter(x=new_df['date'], y=new_df['ema'], line=dict(color = 'pink'), name= "Exponential MA"))
-        plot_div = plot(fig, output_type='div')
+            # Plot Figure + Trend Lines using MA and EMA
+            fig = go.Figure(data=candle, layout=layout)
+            if MA:      
+                new_df['ma'] = new_df['close'].rolling(window=5).mean()
+                fig.add_trace(go.Scatter(x=new_df['date'], y=new_df['ma'], line=dict(color = 'yellow'), name= "Moving Average"))
+            if EMA:
+                new_df['ema'] = new_df['close'].ewm(span=10).mean()
+                fig.add_trace(go.Scatter(x=new_df['date'], y=new_df['ema'], line=dict(color = 'blue'), name= "Exponential MA"))
+            plot_div = plot(fig, output_type='div')
 
-        context = {
-                'plot_div': plot_div
-        }
-            
-        return render(request, "home.html", context)
+            #Add plot to home html page
+            context = {
+                    'plot_div': plot_div
+            }
+                
+            return render(request, "home.html", context)
     else:
         # Homepage initial message before plotting the graph
-        plot_div = "Your Plot is going here! Just choose a PM value, a Start Date and an End Date to plot your chart"
+        plot_div = "Your Plot is going here!"
 
         context = {
             'plot_div': plot_div,
